6be47eda1b43f1591a85a84902f97df2c5f4d013
JoseLagunas <67912656+JoseLagunas@users.noreply.github.com>
2020-07-23 20:38:52 -0500

    Base de datos ya corregida

--- Proyecto.sql
+++ Proyecto.sql
@@ -8,17 +8,31 @@ contrasena varchar(8));
 
 
 Create table animal(id_animal int not null auto_increment primary key, 
+id_usuario int,
 nombre varchar(50),
  estado varchar(50),
- edad varchar(50));
+ edad varchar(50)
+);
+ alter table animal add constraint id_usuario_fk foreign key (id_usuario) references usuario (id_usuario);
  
 Create table limitaciones(id_limitaciones int not null auto_increment primary key,
-distancia_maxima_permitida varchar(50),
+id_animal int,
+distancia_maxima_permitida int,
 ubicacion_geografica varchar(50));
+ alter table limitaciones add constraint id_animal_fk foreign key (id_animal) references animal (id_animal);
 
 Create table rastreador(id_rastreador int not null auto_increment primary key,
-latitud varchar(50),
-longitud varchar(50),
-ultima_posicion varchar(50),
-hora datetime);
- 
+id_animal int,
+latitud float,
+longitud float,
+hora timestamp);
+alter table rastreador add constraint id_animal1_fk foreign key (id_animal) references animal (id_animal);
+
+ create table reporte(id_reporte int not null auto_increment primary key,
+ id_rastreador int,
+ ultima_posicion varchar(50),
+ hora timestamp);
+ alter table reporte add constraint id_rastreador_fk foreign key (id_rastreador) references rastreador (id_rastreador);
+
+select * from usuario;
+select * from reporte;
