9b82050728135d0a133d47084bbc3a5cb626275a
JoseLagunas <67912656+JoseLagunas@users.noreply.github.com>
2020-07-24 20:02:37 -0500

    comit

--- Proyecto.sql
+++ Proyecto.sql
@@ -1,3 +1,5 @@
+
+/*DROP DATABASE Proyecto;*/
 create database Proyecto;
 use Proyecto;
 Create table usuario (id_usuario int not null auto_increment primary key, 
@@ -19,7 +21,7 @@ Create table limitaciones(id_limitaciones int not null auto_increment primary ke
 id_animal int,
 distancia_maxima_permitida int,
 ubicacion_geografica varchar(50));
- alter table limitaciones add constraint id_animal_fk foreign key (id_animal) references animal (id_animal);
+alter table limitaciones add constraint id_animal_fk foreign key (id_animal) references animal (id_animal);
 
 Create table rastreador(id_rastreador int not null auto_increment primary key,
 id_animal int,
@@ -34,5 +36,12 @@ alter table rastreador add constraint id_animal1_fk foreign key (id_animal) refe
  hora timestamp);
  alter table reporte add constraint id_rastreador_fk foreign key (id_rastreador) references rastreador (id_rastreador);
 
+select * from animal;
+select * from limitaciones;
 select * from usuario;
-select * from reporte;
+select * from rastreador;
+
+SELECT animal.id_animal, animal.nombre, animal.estado, animal.edad,  limitaciones.distancia_maxima_permitida
+FROM animal
+LEFT JOIN limitaciones ON animal.id_animal=limitaciones.id_limitaciones;
+ 
